5c88d4a7fa38fba33d70eaa3254a47b2b618bed6
paul88vera <paul88vera@gmail.com>
2022-01-09 15:49:49 -0600

    os update fix

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
 
 // const printProfileData = profileDataArr => {
 //   // This...
@@ -14,6 +16,23 @@ const profileDataArgs = process.argv.slice(2);
 
 // printProfileData(profileDataArgs);
 
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-console.log(generatePage('Jane', 'janehub'));
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+console.log(name, github);
+console.log(generatePage(name, github));
