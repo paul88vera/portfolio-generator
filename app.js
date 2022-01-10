4cc9ffbd6883c682b4889f62a948c9e0164f69fb
paul88vera <paul88vera@gmail.com>
2022-01-09 19:10:11 -0600

    inquirer added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-fs.writeFile('index.html', generatePage(name, github), err =>{
-  if (err) throw new Error(err);
-  console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('index.html', generatePage(name, github), err =>{
+//   if (err) throw new Error(err);
+//   console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
